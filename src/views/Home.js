4971d3a6018999eb2af6d3c03087cc05864d3138
jiru-shan <90534758+jiru-shan@users.noreply.github.com>
2023-06-06 13:59:12 -0700

    Update Home.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import Popup from 'reactjs-popup';
 
-
-function Home() {
-    return(<div>hi</div>);
+function Home() 
+{
+    return(
+    <div>
+        <Popup trigger={<button> Trigger</button> } position="center" modal>
+        <div>Popup content here !!</div>
+        </Popup>
+    </div>);
 }
 
 export default Home;
