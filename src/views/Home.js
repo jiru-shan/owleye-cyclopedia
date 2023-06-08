4666d71c3ed5d4ec7189b48790f378f0f2018814
jiru-shan <hikikomorishan@gmail.com>
2023-06-08 03:09:47 -0700

    save me

--- src/views/Home.js
+++ src/views/Home.js
@@ -42,13 +42,13 @@ function Home()
                 <Stack direction="horizontal">
                     <div className='dividerSmall'></div>
                     <div>
-                        <button className="mainButton" onClick={()=> setModalState(4)}>
-                            <Image src={'https://png.pngtree.com/png-vector/20190411/ourmid/pngtree-vector-resume-icon-png-image_927259.jpg'} roundedCircle fluid/>
+                        <button className="mainButtonS" onClick={()=> setModalState(4)}>
+                            <Image className="mainImage" src={'https://cdn4.iconfinder.com/data/icons/documents-letters-and-stationery/400/doc-10-512.png'} thumbnail fluid/>
                         </button>
                     </div>
                     <div className='ms-auto '>
-                        <button className="mainButton" onClick={()=> setModalState(5)}>
-                            <Image src={'./resources/owl-eyes.jpg'} roundedCircle fluid/>
+                        <button className="mainButtonS" onClick={()=> setModalState(5)}>
+                            <Image className="mainImage" src={'https://t4.ftcdn.net/jpg/02/57/53/83/360_F_257538375_qUSk34MmbcbEm62JanJf4IV3c1PYhq4O.jpg'} fluid/>
                         </button>
                     </div>
                     <div className='dividerSmall'></div>
@@ -61,24 +61,24 @@ function Home()
             <Col className="firstCol">
                 <Stack className="middleButtonStack" direction="horizontal">
                     <div>
-                        <button className="mainButton" onClick={()=> setModalState(1)}>
-                            <Image src={'./resources/owl-eyes.jpg'} roundedCircle fluid/>
+                        <button className="mainButtonS" onClick={()=> setModalState(1)}>
+                            <Image className="mainImage" src={'https://assets.iflscience.com/assets/articleNo/67775/aImg/66118/vintage-books-m.jpg'} fluid/>
                         </button>
                     </div>
                     <div className='divider'>
                     <hr className='line'></hr>
                     </div>
                     <div>
-                        <button className="mainButton" onClick={()=> setModalState(2)}>
-                            <Image src={'./resources/owl-eyes.jpg'} roundedCircle fluid/>
+                        <button className="mainButtonS" onClick={()=> setModalState(2)}>
+                            <Image className="mainImage" src={'https://img1.cgtrader.com/items/110538/25a195d58b/large/vintage-car-wheel-3d-model-max-obj-fbx-mtl.png'} fluid/>
                         </button>
                     </div>
                     <div className='divider'>
                     <hr className='line'></hr>
                     </div>
                     <div>
-                        <button className="mainButton" onClick={()=> setModalState(3)}>
-                        <Image src={'./resources/owl-eyes.jpg'} roundedCircle fluid/>
+                        <button className="mainButtonS" onClick={()=> setModalState(3)}>
+                        <Image className="mainImage" src={'https://images.ctfassets.net/4ivszygz9914/3d3d0509-5150-4ec0-a129-a82b8af60881/10e6da902a2e13f69cb8ec05e7f44703/2e0679f7-393e-4966-84bb-7450075a4f02.jpeg?fm=webp'} fluid/>
                         </button>
                     </div>
                 </Stack>
@@ -92,13 +92,13 @@ function Home()
                 <Stack direction="horizontal">
                     <div className='dividerSmall'></div>
                     <div>
-                        <button className="mainButton" onClick={()=> setModalState(6)}>
-                            <Image src={'./resources/owl-eyes.jpg'} roundedCircle fluid/>
+                        <button className="mainButtonS" onClick={()=> setModalState(6)}>
+                            <Image className="mainImage" src={'https://timvandevall.com/wp-content/uploads/2013/07/Plot-Diagram-Worksheet-01.png'} thumbnail fluid/>
                         </button>
                     </div>
                     <div className='ms-auto'>
-                        <button className="mainButton" onClick={()=> setModalState(7)}>
-                            <Image src={'./resources/owl-eyes.jpg'} roundedCircle fluid/>
+                        <button className="mainButtonS" onClick={()=> setModalState(7)}>
+                            <Image className="mainImage" thumbnail src={'https://i.etsystatic.com/34565277/r/il/4bd024/3783408258/il_fullxfull.3783408258_jhef.jpg'} fluid/>
                         </button>
                     </div>
                     <div className='dividerSmall'></div>
